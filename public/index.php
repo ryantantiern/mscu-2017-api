77e95f2a39658cfb070edffa32f7f0c9a21c3c04
ryantantiern <ryantantiern@gmail.com>
2017-02-01 13:24:23 +0000

    add Header set .. in .htaccess

--- public/index.php
+++ public/index.php
@@ -7,8 +7,6 @@
  * @author   Taylor Otwell <taylor@laravel.com>
  */
 header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
-header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token');
 /*
 |--------------------------------------------------------------------------
 | Register The Auto Loader
