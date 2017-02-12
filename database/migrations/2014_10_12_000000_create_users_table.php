69b09a3a84490db6045f32fff692dd7624279233
ryantantiern <ryantantiern@gmail.com>
2017-02-12 13:48:17 +0000

    update users table to hold photo path location

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -21,6 +21,7 @@ class CreateUsersTable extends Migration
             $table->string('phone')->nullable();
             $table->string('email')->unique();
             $table->string('password');
+            $table->string('photo_path')->nullable();
             $table->timestamps();
         });
     }
